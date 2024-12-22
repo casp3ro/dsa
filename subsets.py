5ab8fb81f918d50a2ffcd68fa0a3b16eaf20567d
Kacper Michalik <kacpermichalik3@gmail.com>
2024-12-22 21:38:20 +0100

    feat: subsets

---
+++ subsets.py
@@ -0,0 +1,17 @@
+def subsets(nums: list[int]) -> list[int]:
+    output = []
+    subsets = []
+
+    def dfs(index):
+        if index >= len(nums):
+            output.append(subsets.copy())
+            return
+
+        subsets.append(nums[index])
+        dfs(index + 1)
+
+        subsets.pop()
+        dfs(index + 1)
+
+    dfs(0)
+    return output
