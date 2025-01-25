b198890f473aff234741a5e52d724585c489bf82
Kacper Michalik <kacpermichalik3@gmail.com>
2025-01-25 14:36:29 +0100

    imp: linked list

--- linked_list.py
+++ linked_list.py
@@ -213,6 +213,24 @@ class LinkedList:
             nNode = nNode.next
             
         return curr.value
+    
+    def remove_nth_from_end(head:Node, n:int)-> Node:
+        dummy = Node(0,head)
+        
+        left = dummy
+        right = dummy
+        
+        for _ in range(n):
+            if right.next:
+                right = right.next
+                
+        while right.next:
+            left = left.next
+            right = right.next
+        
+        left.next = left.next.next
+        
+        return dummy.next
             
         
         
