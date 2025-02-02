9f97a7eb7e2d2cc7fe965100ec2b04e0ada0064e
Kacper Michalik <kacpermichalik3@gmail.com>
2025-02-02 17:36:53 +0100

    feat: find_median_sorted_arrays

---
+++ find_median_sorted_arrays.py
@@ -0,0 +1,29 @@
+def find_median_sorted_arrays(nums1:list[int], nums2:list[int]):
+        A,B = nums1,nums2
+        total = len(A) + len(B)
+        half = total //2
+
+        if len(A) > len(B):
+            A,B = B,A
+
+        l,r = 0, len(A) - 1
+
+        while True:
+            i = (l + r) //2
+            j = half - i -2
+
+            aLeft = A[i] if i >= 0 else float('-inf')
+            aRight = A[i + 1] if i+1 < len(A) else float('inf')
+
+            bLeft = B[j] if j >= 0 else float('-inf')
+            bRight = B[j + 1] if j+1 < len(B) else float('inf')
+
+            if aLeft <= bRight and bLeft <= aRight:
+                if total %2 :
+                    return min(aRight,bRight)
+                return (max(aLeft,bLeft) + min(aRight, bRight)) / 2
+            
+            elif aLeft > bRight:
+                 r = i -1
+            else:
+                l = i +1
